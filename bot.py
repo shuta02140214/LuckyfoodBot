b07f01d9027de8200c1bfc662350eae71d25f22f
shuta02140214 <shuta02140214@gmail.com>
2026-03-14 21:55:36 +0900

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,32 @@
+import discord
+import random
+import datetime
+from discord.ext import tasks, commands
+import os
+
+TOKEN = os.getenv("TOKEN")
+CHANNEL_ID = 1408765577350086810
+
+foods = [
+    "カレー", "ラーメン", "オムライス", "寿司",
+    "ハンバーグ", "唐揚げ", "うどん", "ピザ"
+]
+
+intents = discord.Intents.default()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+@bot.event
+async def on_ready():
+    print("Bot起動")
+    lucky_food.start()
+
+@tasks.loop(minutes=1)
+async def lucky_food():
+    now = datetime.datetime.now()
+
+    if now.hour == 7 and now.minute == 0:
+        channel = bot.get_channel(CHANNEL_ID)
+        food = random.choice(foods)
+        await channel.send(f"☀️ 今日のラッキーフードは **{food}** です！")
+
+bot.run(TOKEN)
